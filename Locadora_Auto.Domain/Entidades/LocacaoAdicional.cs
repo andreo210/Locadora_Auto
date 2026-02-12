abe742feb63e8d8f211c0a684fada8811468da63
andreo210 <98775473+andreo210@users.noreply.github.com>
2026-02-12 17:50:48 -0300

    ok

--- Locadora_Auto.Domain/Entidades/LocacaoAdicional.cs
+++ Locadora_Auto.Domain/Entidades/LocacaoAdicional.cs
@@ -2,12 +2,35 @@
 {
     public class LocacaoAdicional
     {
-        public int IdLocacao { get; set; }
-        public int IdAdicional { get; set; }
-        public int Quantidade { get; set; }
+        public int IdLocacaoAdicional { get; private set; }
+        public int IdAdicional { get; private set; }
+        public decimal ValorDiariaContratada { get; private set; }
+        public int Quantidade { get; private set; }
+        public int Dias { get; private set; }
 
-        public Locacao Locacao { get; set; } = null!;
-        public Adicional Adicional { get; set; } = null!;
+        protected LocacaoAdicional() { }
+
+        public static LocacaoAdicional Criar(
+            int idAdicional,
+            decimal valorDiaria,
+            int quantidade,
+            int dias)
+        {
+            if (quantidade <= 0)
+                throw new DomainException("Quantidade inválida");
+
+            return new LocacaoAdicional
+            {
+                IdAdicional = idAdicional,
+                ValorDiariaContratada = valorDiaria,
+                Quantidade = quantidade,
+                Dias = dias
+            };
+        }
+
+        public decimal CalcularTotal()
+            => ValorDiariaContratada * Quantidade * Dias;
     }
+
 
 }
