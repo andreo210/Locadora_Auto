6837d3be68699d48b022eb784184f6baeba627fa
Andre <andreo210@hotmail.com>
2026-02-23 10:21:39 -0300

    layout com thema dark

---
+++ Locadora_Auto.Front/wwwroot/js/Layout.js
@@ -0,0 +1,30 @@
+﻿
+
+window.themeManager = {
+
+    setTheme: function (isDark) {
+        if (isDark) {
+            document.body.classList.add("dark-theme");
+            document.body.classList.remove("light-theme");
+            localStorage.setItem("theme", "dark");
+        } else {
+            document.body.classList.add("light-theme");
+            document.body.classList.remove("dark-theme");
+            localStorage.setItem("theme", "light");
+        }
+    },
+
+    loadTheme: function () {
+        const savedTheme = localStorage.getItem("theme");
+
+        if (savedTheme === "dark") {
+            document.body.classList.add("dark-theme");
+            document.body.classList.remove("light-theme");
+            return true;
+        } else {
+            document.body.classList.add("light-theme");
+            document.body.classList.remove("dark-theme");
+            return false;
+        }
+    }
+};
